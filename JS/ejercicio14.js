c7c234b403c4a192904944bd035b9fbed9a37328
MfrMesa <berriesccs@gmail.com>
2024-01-09 12:53:47 +0100

    js

---
+++ JS/ejercicio14.js
@@ -0,0 +1,69 @@
+// # Array Methods Advanced
+// Create a function called `uncompletedNotes` that returns only not completed todos.
+// **Suggestion**
+// Use the forEach and filter arrays methods.
+
+const notes = [
+    {
+        id: 1,
+        description: "Workout program",
+        todos: [
+        {
+            id: 1,
+            name: "Push ups - 10 x 3",
+            done: false,
+        },
+        {
+            id: 2,
+            name: "Abdominals - 20 x 3",
+            done: true,
+        },
+        {
+            id: 3,
+            name: "Tapis Roulant - 15min",
+            done: true,
+        },
+    ],
+    },
+    {
+        id: 2,
+        description: "Front-end Roadmap",
+        todos: [
+        {
+            id: 1,
+            name: "Learn HTML",
+            done: true,
+        },
+        {
+            id: 2,
+            name: "Learn CSS",
+            done: true,
+        },
+        {
+            id: 3,
+            name: "Learn JavaScript",
+            done: true,
+        },
+        {
+            id: 4,
+            name: "Learn Angular",
+            done: false,
+        },
+    ],
+    },
+];
+
+
+function uncompletedNotes(notes) {
+    let todosIncompleto = [];
+
+    notes.forEach((notas) => {
+        const incompleto = notas.todos.filter((todo) => !todo.done);
+        todosIncompleto = todosIncompleto.concat(incompleto);
+    });
+
+    return todosIncompleto;
+}
+
+const incompleto = uncompletedNotes(notes);
+console.log(incompleto);
---
+++ JS/ejercicio15.js
@@ -0,0 +1,25 @@
+// # Array Methods Advanced
+// Create a function called `sortPeopleByAge` that returns the people in ascending order by age.
+
+
+const people = [
+    { name: 'Paul', age: 16 },
+    { name: 'George', age: 17 },
+    { name: 'Lucas', age: 21 },
+    { name: 'Marco', age: 32 },
+    { name: 'Peter', age: 18 },
+    { name: 'Carl', age: 13 },
+    { name: 'Simon', age: 24 },
+    { name: 'Mark', age: 15 },
+    { name: 'Sandra', age: 34 },
+    { name: 'Alice', age: 28 }
+];
+
+function sortPeopleByAge(people) {
+    let sorted = people.sort ((a, b) => a.age - b.age);
+    console.log(sorted);
+    return sorted;
+}
+
+const sortingByAge = sortPeopleByAge(people)
+console.log(sortingByAge)
