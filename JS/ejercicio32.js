bdba471f5c4010e30ef0af49db61e78d98a054a0
MfrMesa <berriesccs@gmail.com>
2024-01-25 17:57:44 +0100

    .

--- JS/ejercicio32.js
+++ JS/ejercicio32.js
@@ -8,12 +8,14 @@ const person = {
     age: 25,
 };
 
-
-const filtro = {
-    id: person.id,
-    age: person.age
+function replacer (key, value) {
+    if (typeof value === "string") {
+        return undefined;
+    }
+    return value;
 }
 
-const json = JSON.stringify(filtro);
+const json = JSON.stringify(person, replacer);
+
 
     console.log(json); // Should return: { id: 1, age: 25 }
