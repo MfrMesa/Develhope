b79ba8e0f9a01f71b88c4c92a2d4ec79bccfc2f3
MfrMesa <berriesccs@gmail.com>
2024-01-30 14:07:02 +0100

    .

--- JS/ejercicio35.js
+++ JS/ejercicio35.js
@@ -3,13 +3,22 @@
 // The repeatHello function will have to execute the callback function with an interval of 1 second. The callback function must 
 // be an arrow function, can you also explain why?
 
-function repeatHello() {
-    let saludo = "Hello";
-    setInterval(() => {
-        console.log(saludo);
-    }, 1000);
+// function repeatHello() {
+//     let saludo = "Hello";
+//     setInterval(() => {
+//         console.log(saludo);
+//     }, 1000);
+// }
+
+// repeatHello();
+
+// version corta
+
+
+function repeatHello(callback) {
+    setInterval(callback, 1000);
 }
 
-repeatHello();
+repeatHello(() => console.log("Hello"));
 
-// El código establece un intervalo que imprime "Hello" en la consola cada segundo.
+// El código establece un intervalo que imprime "Hello" en la consola cada segundo hasta que se detenga la ejecución del programa. 
