7e38d4ffc89ae6d4850b8ee691c06379590933da
MfrMesa <berriesccs@gmail.com>
2024-01-30 14:20:15 +0100

    .

--- JS/ejercicio37.js
+++ JS/ejercicio37.js
@@ -2,10 +2,9 @@
 // Write a `promise`. If the variable called `number` is greater than 10, then it returns a resolve, otherwise it returns a reject.
 // Then manage the promise with `then` and `catch` in case of any reject.
 
-
 const number = 15;
 
-let myPromise = new Promise (function (resolve, reject) {
+let myPromise = new Promise ((resolve, reject) => {
     if (number > 10) {
         resolve(number);
     } else {
